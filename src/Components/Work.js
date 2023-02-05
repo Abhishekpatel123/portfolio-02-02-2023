1f80520fed2d681fe1713d31374dab4b705ffcdc
Abhishek <abhipatel8719@gmail.com>
2023-02-05 20:46:49 +0530

    work exp work icon bug

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -33,7 +33,7 @@ function Work({
             >
               <path d="M3.33331 1.5V10.5H7.33331V8.75H8.66665V10.5H12.6666V1.5H3.33331ZM4.66665 2.5H5.99998V3.5H4.66665V2.5ZM7.33331 2.5H8.66665V3.5H7.33331V2.5ZM9.99998 2.5H11.3333V3.5H9.99998V2.5ZM4.66665 4.5H5.99998V5.5H4.66665V4.5ZM7.33331 4.5H8.66665V5.5H7.33331V4.5ZM9.99998 4.5H11.3333V5.5H9.99998V4.5ZM4.66665 6.5H5.99998V7.5H4.66665V6.5ZM7.33331 6.5H8.66665V7.5H7.33331V6.5ZM9.99998 6.5H11.3333V7.5H9.99998V6.5ZM4.66665 8.5H5.99998V9.5H4.66665V8.5ZM9.99998 8.5H11.3333V9.5H9.99998V8.5Z" />
             </svg>
-            <p className="text-content text-xs md:text-sm font-light pl-1">
+            <p className=" text-content text-xs md:text-sm font-light pl-1">
               {company}
             </p>
           </div>
@@ -62,12 +62,9 @@ function Work({
       {/* detailed */}
       <div className="mt-4">
         {points?.map((point, index) => (
-          <div className="flex items-center ml-4" key={`point-${index}`}>
-            <FaHandPointRight />
-            <p
-              key={`point-${index}`}
-              className="text-content text-sm md:text-sm font-light pl-2"
-            >
+          <div className="flex  ml-4" key={`point-${index}`}>
+            <FaHandPointRight className="" />
+            <p className="flex-1 text-content text-sm md:text-sm font-light pl-2">
               {point}
             </p>
           </div>
