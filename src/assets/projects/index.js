890f8a65af8c18dc2873d948393d9f451275f794
Abhishek <abhipatel8719@gmail.com>
2023-02-13 00:16:38 +0530

    Feat - add old portfolio in projdect

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,7 +1,18 @@
-import organicStore1 from "./organic-store/organic-store1.jpeg";
-import organicStore2 from "./organic-store/organic-store2.jpeg";
+import organicStore1 from './organic-store/organic-store1.jpeg';
+import organicStore2 from './organic-store/organic-store2.jpeg';
+import portfolio1 from './portfolio/portfolio1.png';
+import portfolio2 from './portfolio/portfolio2.png';
+import portfolio3 from './portfolio/portfolio3.png';
+import portfolio4 from './portfolio/portfolio4.png';
 
 export const organicStoreImages = [
-  { name: "Frontend", url: organicStore1 },
-  { name: "Frontend", url: organicStore2 },
+  { name: 'Frontend', url: organicStore1 },
+  { name: 'Frontend', url: organicStore2 },
+];
+
+export const portfolioImages = [
+  { name: 'Frontend', url: portfolio1 },
+  { name: 'Frontend', url: portfolio2 },
+  { name: 'Frontend', url: portfolio3 },
+  { name: 'Frontend', url: portfolio4 },
 ];
--- src/db/project.db.js
+++ src/db/project.db.js
@@ -1,80 +1,99 @@
-import { DiCss3 } from "react-icons/di";
+import { DiCss3, DiReact } from 'react-icons/di';
 import {
   SiExpress,
   SiHtml5,
   SiMongodb,
   SiNodedotjs,
   SiSocketdotio,
-} from "react-icons/si";
-import { TbBrandJavascript } from "react-icons/tb";
-import { strangerTalk } from "../assets";
-import { organicStoreImages } from "../assets/projects";
+  SiTypescript,
+} from 'react-icons/si';
+import { TbBrandJavascript } from 'react-icons/tb';
+import { FaAngular } from 'react-icons/fa';
+import { strangerTalk } from '../assets';
+import { organicStoreImages, portfolioImages } from '../assets/projects';
 
 export const projectType = {
-  ALL: "ALL",
-  WEB: "WEB",
-  APP: "APP",
+  ALL: 'ALL',
+  WEB: 'WEB',
+  APP: 'APP',
 };
 
 const projects = [
   {
-    id: "1",
-    title: "Organic Store",
+    id: '1',
+    title: 'Organic Store',
     type: projectType.APP,
-    status: "Working",
+    status: 'Working',
     tech_usages: [
-      { label: "HTML", Icon: SiHtml5 },
-      { label: "CSS", Icon: DiCss3 },
-      { label: "JAVASCRIPT", Icon: TbBrandJavascript },
-      { label: "NODE JS", Icon: SiNodedotjs },
-      { label: "EXPRESS JS", Icon: SiExpress },
-      { Icon: SiMongodb, label: "MONGODB" },
-      { label: "SOCKET.IO", Icon: SiSocketdotio },
+      { label: 'HTML', Icon: SiHtml5 },
+      { label: 'CSS', Icon: DiCss3 },
+      { label: 'JAVASCRIPT', Icon: TbBrandJavascript },
+      { label: 'Typescript', Icon: SiTypescript },
+      { label: 'Angular', Icon: FaAngular },
+      { label: 'NODE JS', Icon: SiNodedotjs },
+      { label: 'EXPRESS JS', Icon: SiExpress },
+      { Icon: SiMongodb, label: 'MONGODB' },
     ],
     description:
-      "Designed and developed a fully dynamic web application to dynamically update each section. Includes all the e-commerce features like Wishlist, cart, payment history, order, etc.",
-    liveLink: "https://comment-web-app.herokuapp.com",
-    githubLink: "https://github.com/Abhishekpatel123/organic-store-server",
+      'Designed and developed a fully dynamic web application to dynamically update each section. Includes all the e-commerce features like Wishlist, cart, payment history, order, etc.',
+    liveLink: 'https://comment-web-app.herokuapp.com',
+    githubLink: 'https://github.com/Abhishekpatel123/organic-store-server',
     images: organicStoreImages,
   },
   {
-    id: "2",
-    title: "Live Comment Web App",
-    status: "Completed",
+    id: '2',
+    title: 'Live Comment Web App',
+    status: 'Completed',
     type: projectType.WEB,
     tech_usages: [
-      { label: "HTML", Icon: SiHtml5 },
-      { label: "CSS", Icon: DiCss3 },
-      { label: "JAVASCRIPT", Icon: TbBrandJavascript },
-      { label: "NODE JS", Icon: SiNodedotjs },
-      { label: "EXPRESS JS", Icon: SiExpress },
-      { Icon: SiMongodb, label: "MONGODB" },
-      { label: "SOCKET.IO", Icon: SiSocketdotio },
+      { label: 'HTML', Icon: SiHtml5 },
+      { label: 'CSS', Icon: DiCss3 },
+      { label: 'JAVASCRIPT', Icon: TbBrandJavascript },
+      { label: 'NODE JS', Icon: SiNodedotjs },
+      { label: 'EXPRESS JS', Icon: SiExpress },
+      { Icon: SiMongodb, label: 'MONGODB' },
+      { label: 'SOCKET.IO', Icon: SiSocketdotio },
     ],
     description:
-      "Developed a fully functional real-time chat application using Node.js, socket.io, and Mustache. mplemented the concept of room-based segregation of users with a unique display name and room name. It has an option for sending location through chat messages, location is made by concatenating coordinates in the URL of google maps. Also, Profanity is not allowed, those words are blocked and are not sent.",
-    liveLink: "https://comment-web-app.herokuapp.com",
-    githubLink: "https://comment-web-app.herokuapp.com",
-    images: [{ url: strangerTalk, name: "Frontend Home Page" }],
+      'Developed a fully functional real-time chat application using Node.js, socket.io, and Mustache. mplemented the concept of room-based segregation of users with a unique display name and room name. It has an option for sending location through chat messages, location is made by concatenating coordinates in the URL of google maps. Also, Profanity is not allowed, those words are blocked and are not sent.',
+    liveLink: 'https://comment-web-app.herokuapp.com',
+    githubLink: 'https://comment-web-app.herokuapp.com',
+    images: [{ url: strangerTalk, name: 'Frontend Home Page' }],
   },
   {
-    id: "1",
-    title: "PathPradarshak",
-    status: "Working",
+    id: '3',
+    title: 'PathPradarshak',
+    status: 'Working',
     type: projectType.WEB,
     tech_usages: [
-      { label: "HTML", Icon: SiHtml5 },
-      { label: "CSS", Icon: DiCss3 },
-      { label: "JAVASCRIPT", Icon: TbBrandJavascript },
-      { label: "NODE JS", Icon: SiNodedotjs },
-      { label: "EXPRESS JS", Icon: SiExpress },
-      { Icon: SiMongodb, label: "MONGODB" },
-      { label: "SOCKET.IO", Icon: SiSocketdotio },
+      { label: 'HTML', Icon: SiHtml5 },
+      { label: 'CSS', Icon: DiCss3 },
+      { label: 'JAVASCRIPT', Icon: TbBrandJavascript },
+      { label: 'React JS', Icon: DiReact },
+      { label: 'NODE JS', Icon: SiNodedotjs },
+      { label: 'EXPRESS JS', Icon: SiExpress },
+      { Icon: SiMongodb, label: 'MONGODB' },
     ],
-    description: "Ab pade kam paise mai or gher mai rehete hue",
-    liveLink: "https://comment-web-app.herokuapp.com",
-    githubLink: "https://comment-web-app.herokuapp.com",
+    description: 'Ab pade kam paise mai or gher mai rehete hue',
+    liveLink: 'https://comment-web-app.herokuapp.com',
+    githubLink: 'https://comment-web-app.herokuapp.com',
     images: [],
+  },
+  {
+    id: '4',
+    title: 'Portfolio',
+    status: 'Completed',
+    type: projectType.WEB,
+    tech_usages: [
+      { label: 'HTML', Icon: SiHtml5 },
+      { label: 'CSS', Icon: DiCss3 },
+      { label: 'JAVASCRIPT', Icon: TbBrandJavascript },
+      { label: 'React JS', Icon: DiReact },
+    ],
+    description: 'My personal old portfolio.',
+    liveLink: 'https://abhishekpatel8719.netlify.app/',
+    githubLink: 'https://github.com/Abhishekpatel123/My-Portfolio',
+    images: portfolioImages,
   },
 ];
 
