bdf2943a8ea2f26688fdc6f21d9aaf041b4bce11
Abhishek Patel <abhipatel8719@gmail.com>
2024-05-26 16:31:49 +0530

    Fix

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = '/portfolio-02-02-2023';
-export const BASE_URL = process.env?.PUBLIC_URL || '/portfolio2.0';
+export const BASE_URL = process.env?.PUBLIC_URL;
 export const RESUME_DRIVE_URL = process.env.REACT_APP_RESUME_DRIVE_URL;
 
 export const projectType = {
